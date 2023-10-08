5753611a0d8bfc2fa288201d44f18bc3e3bd43c5
abd251 <81411376+abd251@users.noreply.github.com>
2023-10-08 09:53:03 +0300

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Abdulhakim | Personal Portfolio",
+  description: "Abdulhakim is a full-stack developer with skill and expireance.",
 };
 
 export default function RootLayout({
