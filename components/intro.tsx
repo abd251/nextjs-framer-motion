83d5b8ffa017ef101d766c83de8a607a66d50088
abd251 <81411376+abd251@users.noreply.github.com>
2023-10-08 13:57:10 +0300

    ...

--- components/intro.tsx
+++ components/intro.tsx
@@ -32,7 +32,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src=""
+              src="/profile.png"
               alt="Ricardo portrait"
               width="192"
               height="192"
